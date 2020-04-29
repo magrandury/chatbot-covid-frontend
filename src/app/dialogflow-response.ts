16c62fc772b942fa203819fcba821b2b1719a25d
Maria Grandury <mariagrandury@gmail.com>
2020-04-29 02:12:48 +0200

    [dialogflow-response.ts] Add file

---
+++ src/app/dialogflow-response.ts
@@ -0,0 +1,25 @@
+export interface DialogflowResponse {
+  fulfillmentMessages: Array<
+    {
+      platform: string,
+      message: string,
+      text?: {
+        text: Array<string>
+      },
+      quickReplies?: {
+        quickReplies: Array<string>,
+        title: string
+      },
+      card?: {
+        buttons: Array<{
+          text: string,
+          postback: string
+        }>,
+        title: string,
+        subtitle: string,
+        imageUri: string
+      }
+    }>;
+  fulfillmentText: string;
+  sessionId: string;
+}
